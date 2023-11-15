4e937978e9d87347bfeb1b9bbb398346e188dda3
JavierEstudios <xjfaravi975@ieshnosmachado.org>
2023-11-15 11:08:28 +0100

    Final?

--- task/models.py
+++ task/models.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.db import models
 
 class Task (models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField
-    done = models.BooleanField
+    name = models.CharField(max_length=50, default="")
+    description = models.TextField(default="")
+    done = models.BooleanField(default=False)
 
-
-# Create your models here.
+    def __str__(self):
+        return self.name
